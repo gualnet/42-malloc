9c99a128e9150d36f2e347c41278e2979564143e
Guillaume ALY <galy@e2r11p1.42.fr>
2018-05-03 17:25:43 +0200

    rework 2 pass

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #  !/bin/sh
-
-
-#export DEBUG_MALLOC=[TRUE|FALSE]
-export DEBUG_MALLOC=FALSE
-
 export DYLD_LIBRARY_PATH=.
-export DYLD_INSERT_LIBRARIES="libft_malloc_x86_64_Darwin.so"
+export DYLD_INSERT_LIBRARIES="libft_malloc.so"
 export DYLD_FORCE_FLAT_NAMESPACE=1
 $@
